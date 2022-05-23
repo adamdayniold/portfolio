06b4f803160bfbdc290f19d768e268f50ca4135a
adam dayniold <adamdayniold@talentcloud.ai>
2022-05-23 20:20:14 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          ADAM's profile using reactjs
+          {/* Edit <code>src/App.js</code> and save to reload. */}
         </p>
         <a
           className="App-link"
