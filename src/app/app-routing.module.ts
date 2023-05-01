1b2656943f180da9a98e7c5efb3917f549cc6bd0
Adam Dayniold <adamdayniold@talentcloud.ai>
2023-05-01 11:28:29 +0800

    update to angular

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { DescriptionComponent } from './modules/description/description.component';
+
+const appRoutes: Routes = [
+  { path: '', component: DescriptionComponent },
+  { path: '**', pathMatch: 'full', redirectTo: ''}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(appRoutes, { useHash: true })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+  static forRoot: any;
+}
