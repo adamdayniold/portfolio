50b1556207ba389a17b6ae0c76a59120cabd8433
Adam Dayniold <adamdayniold@talentcloud.ai>
2023-05-03 00:42:07 +0800

    update portfolio remove unwanted cache and files

--- src/app/modules/description/description.component.ts
+++ src/app/modules/description/description.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-description',
   templateUrl: './description.component.html',
-  styleUrls: ['./description.component.scss']
+  styleUrls: ['./description.component.scss'],
+  animations: [
+    trigger('introduction-smaller', [
+      state('true', style({
+        opacity: 1
+      })),
+      state('false', style({
+        opacity: 0
+      })),
+      transition('* => *', animate('200ms ease'))
+    ])
+  ]
 })
-export class DescriptionComponent {
+export class DescriptionComponent implements OnInit {
   title = 'adam_portfolio';
+  introduction: boolean = true;
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.introduction = false;
+    }, 1000)
+  }
 }
