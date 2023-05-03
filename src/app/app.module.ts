053573afbad7e1ab949aa480885a3a03cc2e1588
Adam Dayniold <adamdayniold@talentcloud.ai>
2023-05-03 18:33:38 +0800

    update portfolio with fade transition

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 
 import { DescriptionComponent } from './modules/description/description.component';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,7 +16,8 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/modules/description/description.component.ts
+++ src/app/modules/description/description.component.ts
@@ -1,29 +1,30 @@
 import { trigger, state, style, transition, animate } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
+const enterTransition = transition(':enter', [
+  style({ opacity: 0 }),
+  animate('1s ease-in', style({ opacity: 1 }))
+]);
+const fadeIn = trigger('fadeIn', [enterTransition]);
+
 @Component({
   selector: 'app-description',
   templateUrl: './description.component.html',
   styleUrls: ['./description.component.scss'],
-  animations: [
-    trigger('introduction-smaller', [
-      state('true', style({
-        opacity: 1
-      })),
-      state('false', style({
-        opacity: 0
-      })),
-      transition('* => *', animate('200ms ease'))
-    ])
-  ]
+  animations: [fadeIn]
 })
 export class DescriptionComponent implements OnInit {
   title = 'adam_portfolio';
   introduction: boolean = true;
+  fadetrans: boolean = false;
 
   ngOnInit() {
     setTimeout(() => {
       this.introduction = false;
     }, 1000)
+  }
+
+  fadeAnim() {
+    this.fadetrans = true;
   }
 }
