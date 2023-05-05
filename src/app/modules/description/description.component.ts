7899b749c96f5d94c23facf8797ed821127cc4ae
Adam Dayniold <adamdayniold@talentcloud.ai>
2023-05-05 13:32:12 +0800

    update animations

--- src/app/modules/description/description.component.ts
+++ src/app/modules/description/description.component.ts
@@ -1,33 +1,27 @@
 import { trigger, state, style, transition, animate } from '@angular/animations';
+import { fadeInAnimation, hingeAnimation } from 'angular-animations';
 import { Component, OnInit } from '@angular/core';
-
-const enterTransition = transition(':enter', [
-  style({ opacity: 0 }),
-  animate('1s ease-in', style({ opacity: 1 }))
-]);
-const fadeIn = trigger('fadeIn', [enterTransition]);
 
 @Component({
   selector: 'app-description',
   templateUrl: './description.component.html',
   styleUrls: ['./description.component.scss'],
-  animations: [fadeIn]
+  animations: [hingeAnimation(), fadeInAnimation()]
 })
 export class DescriptionComponent implements OnInit {
   title = 'adam_portfolio';
   introduction: boolean = true;
-  fadetrans: boolean = false;
+  hinge: boolean = false;
   timelineOccur: boolean = false;
 
   ngOnInit() {
   }
 
   intro() {
-    this.introduction = false;
-    this.timelineOccur = true;
-  }
-
-  fadeAnim() {
-    this.fadetrans = true;
+    this.hinge = true;
+    setTimeout(() => {
+      this.introduction = false;
+      this.timelineOccur = true;
+    }, 2000);
   }
 }
