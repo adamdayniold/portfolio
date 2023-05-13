b2a4742c5609be8dc4043f149e99102865909e81
Adam Dayniold <adamdayniold@talentcloud.ai>
2023-05-14 01:30:33 +0800

    update latest animation

--- src/app/modules/description/description.component.ts
+++ src/app/modules/description/description.component.ts
@@ -1,18 +1,27 @@
 import { trigger, state, style, transition, animate } from '@angular/animations';
-import { fadeInAnimation, hingeAnimation } from 'angular-animations';
+import { fadeInAnimation, hingeAnimation, lightSpeedOutAnimation, lightSpeedInAnimation, bounceInAnimation, bounceOutAnimation, bounceOutOnLeaveAnimation } from 'angular-animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-description',
   templateUrl: './description.component.html',
   styleUrls: ['./description.component.scss'],
-  animations: [hingeAnimation(), fadeInAnimation()]
+  animations: [
+    hingeAnimation(),
+    fadeInAnimation(),
+    lightSpeedOutAnimation({ duration: 1000, translate: '500px' }),
+    lightSpeedInAnimation(),
+    bounceInAnimation(),
+    bounceOutAnimation(),
+    bounceOutOnLeaveAnimation()
+  ]
 })
 export class DescriptionComponent implements OnInit {
   title = 'adam_portfolio';
   introduction: boolean = true;
   hinge: boolean = false;
   timelineOccur: boolean = false;
+  lightSpeedIn: boolean = false;
 
   ngOnInit() {
   }
@@ -22,7 +31,14 @@ export class DescriptionComponent implements OnInit {
     setTimeout(() => {
       this.introduction = false;
       this.timelineOccur = true;
-    }, 2000);
+      this.lightSpeedInInformation();
+    }, 1010);
+  }
+
+  lightSpeedInInformation() {
+    setTimeout(() => {
+      this.lightSpeedIn = true;
+    }, 1000)
   }
 
 }
